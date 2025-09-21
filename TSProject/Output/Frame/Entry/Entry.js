af8b862aceb6154a8554b6904f9ff18db29026e5
mshentai <zhou235992604@163.com>
2025-09-21 17:55:38 +0800

    移除puerTs

--- TSProject/Output/Frame/Entry/Entry.js
+++
@@ -1,6 +0,0 @@
-import Game from "Script/Scripts/Game";
-function main() {
-    Game.init();
-    Game.dispose();
-}
-main();
--- TSProject/Src/Frame/Entry/Entry.ts
+++
@@ -1,8 +0,0 @@
-import Game from "Script/Scripts/Game";
-
-function main() {
-    Game.init();
-    Game.dispose();
-}
-
-main();
